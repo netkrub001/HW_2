79e997cbd375ae8d66fa03d12d436f05d1107182
netkrub001 <kiritokrub@gmail.com>
2021-12-04 13:29:33 +0700

    Add files via upload

---
+++ HW_TUPLE.py
@@ -0,0 +1,8 @@
+
+days = ("Sunday","Monday","Thesday","Wednesday","Thursday","Friday","Saturday")
+colors = ("red","yellow","pink","green","orange","light" "blue","purple")
+
+for i in range(len(days)):
+    for j in range(len(colors)):
+        print(days[i],":",colors[i], end='\n')
+        break
